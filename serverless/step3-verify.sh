55b67f8a87a82a132bcc98519fce99c0e4528046
maxday <maxime.david@datadoghq.com>
2021-10-05 12:54:36 -0400

    scenarion

--- serverless/step3-verify.sh
+++ serverless/step3-verify.sh
@@ -1,11 +1,12 @@
 if [ -f "/root/workshop/create-urls.js" ]; then
-    cat /root/workshop/create-urls.js | grep "S3Client, PutObjectCommand, GetObjectCommand" || exit 1
-    cat /root/workshop/create-urls.js | grep "module.exports.handler" || exit 1
-    cat /root/workshop/create-urls.js | grep "process.env.BUCKET_NAME" || exit 1
-    cat /root/workshop/create-urls.js | grep "Math.floor" || exit 1
-    cat /root/workshop/create-urls.js | grep "processedOptions" || exit 1
-    cat /root/workshop/create-urls.js | grep "const signedUrl" || exit 1
-    cat /root/workshop/create-urls.js | grep "JSON.stringify" || exit 1
+    cat /root/workshop/create-urls.js | grep -q "S3Client, PutObjectCommand, GetObjectCommand"  || exit 1
+    cat /root/workshop/create-urls.js | grep -q "module.exports.handler" || exit 1
+    cat /root/workshop/create-urls.js | grep -q "process.env.BUCKET_NAME" || exit 1
+    cat /root/workshop/create-urls.js | grep -q "Math.floor" || exit 1
+    cat /root/workshop/create-urls.js | grep -q "processedOptions" || exit 1
+    cat /root/workshop/create-urls.js | grep -q "const signedUrl" || exit 1
+    cat /root/workshop/create-urls.js | grep -q "JSON.stringify" || exit 1
+    echo "done"
 else 
     exit 1
 fi
