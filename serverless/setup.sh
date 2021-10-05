4a2fa7bc3c1fd45f902863b9cbb6dd069db15d78
maxday <maxime.david@datadoghq.com>
2021-10-05 09:29:41 -0400

    scenarion

--- serverless/setup.sh
+++ serverless/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mkdir -p /root/dash-serverless-workshop
-docker pull maxday/dash:2
-docker run -it -v "$(pwd)"/dash-serverless-workshop:/root/dash-serverless-workshop maxday/dash:2
-clear
-echo "All set! You can start the workshop."
+SETUP_ENDPOINT="https://r8or7hfmbh.execute-api.us-east-1.amazonaws.com/366BBD238B78B97F92EAD8B529DA057FC6354B37/maxday-setup"
+RESULT=$( curl -s "$SETUP_ENDPOINT" )
+
+export AWS_DEFAULT_REGION=$( echo $RESULT | jq -r .region )
+export AWS_ACCESS_KEY_ID=$( echo $RESULT | jq -r .key )
+export AWS_SECRET_ACCESS_KEY=$( echo $RESULT | jq -r .secret )
+export DATADOG_API_KEY=$( echo $RESULT | jq -r .apiKey )
