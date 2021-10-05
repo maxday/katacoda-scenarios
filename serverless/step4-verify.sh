979c5516e1265f1daf2f6db127f32f16bb1237f0
maxday <maxime.david@datadoghq.com>
2021-10-05 13:23:03 -0400

    scenarion

--- serverless/step4-verify.sh
+++ serverless/step4-verify.sh
@@ -1,1 +1,11 @@
-jest && echo "done"
+cd /root/workshop
+if [ -f "/usr/bin/jest" ]; then
+    jest
+    if [ "$?" -ne 0 ]; then
+        exit 1
+    else
+        exit 0
+    fi
+else 
+    exit 1
+fi
