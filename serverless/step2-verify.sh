6d0e4b94ad250f612bd8084cf581afdf6a117443
maxday <maxime.david@datadoghq.com>
2021-10-05 11:13:42 -0400

    scenarion

--- serverless/step2-verify.sh
+++ serverless/step2-verify.sh
@@ -1,4 +1,4 @@
-if [ -d "node_modules/@aws-sdk" ]; then
+if [ -d "/root/workshop/node_modules/@aws-sdk" ]; then
     numberOfPackage=$(ls -la node_modules/@aws-sdk | grep -E 'client-s3|s3-request-presigner' | wc -l)
     [ $numberOfPackage -eq "2" ] && echo "done"
 else 
