f261016acfdc77115b75fd803529cf748169eb04
maxday <maxime.david@datadoghq.com>
2021-10-05 10:13:11 -0400

    scenarion

--- serverless/assets/setupLab.sh
+++ serverless/assets/setupLab.sh
@@ -2,7 +2,7 @@
 
 read -p "Please type the first name of your first name + last name \n For instance my name is Maxime David so I'll type mdavid : " stageName
 
-export STAGE=stageName
+export STAGE=$stageName
 
 SETUP_ENDPOINT="https://r8or7hfmbh.execute-api.us-east-1.amazonaws.com/366BBD238B78B97F92EAD8B529DA057FC6354B37/maxday-setup"
 RESULT=$( curl -s "$SETUP_ENDPOINT" )
