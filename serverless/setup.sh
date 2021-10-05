578b88e4ab83293726170f204a53eb549d12c0d8
maxday <maxime.david@datadoghq.com>
2021-10-05 09:37:18 -0400

    scenarion

--- serverless/setup.sh
+++ serverless/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "IN SETUP"
+touch toto
 # SETUP_ENDPOINT="https://r8or7hfmbh.execute-api.us-east-1.amazonaws.com/366BBD238B78B97F92EAD8B529DA057FC6354B37/maxday-setup"
 # RESULT=$( curl -s "$SETUP_ENDPOINT" )
 
